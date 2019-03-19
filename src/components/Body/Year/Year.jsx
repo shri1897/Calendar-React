07919e91af50d890f52a75067d4b75353615e02b
Roshanshah1234 <r9555801907@gmail.com>
2019-03-19 23:11:09 +0530

    ADDED Container for year

--- src/components/Body/Year/Year.jsx
+++ src/components/Body/Year/Year.jsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import './Body/Year/Year.module.css'
+import Styles from './Year.module.css'
+
+
 
 
 class Year extends React.Component {
 
 
     render() {
-        let Container = [];
-        for (let i = 0; i < 12; i++) {
-            return (<div>{i}</div>)
+        let container = [];
 
+        for (let i = 0; i < 12; i++) {
+            container.push(<div key={i}> {i}</div>);
         }
+
         return (
             <>
-                <div className="grid-container">
-                    {/* <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>7</div>
-                    <div>8</div>
-                    <div>9</div>
-                    <div>10</div>
-                    <div>11</div>
-                    <div>12</div> */}
-                    {Container}
+                <div className={Styles.gridContainer}>
+                    {container}
+
                 </div>
 
             </>
